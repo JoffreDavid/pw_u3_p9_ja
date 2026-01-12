a0e60e2d26c2d239f1f4cf0b08dccf69c9fe1a24
JoffreDavid <jdariasb@uce.edu.ec>
2026-01-11 19:14:19 -0500

    Taller23: Path variables y Query params

--- src/views/HolaMundo.vue
+++ src/views/HolaMundo.vue
@@ -1,10 +1,30 @@
 <template>
   <h2>Hola Mundo</h2>
+  <h1>Consultando los datos del Estudiante</h1>
+  <h3>{{ idEstudiante }}</h3>
+  <button v-on:click="botonId()">MostrarId</button>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            idEstudiante: null
+        };
+    },
+    created(){
+        console.log(this.$route);
+        console.log(this.$route.params.id);
+        //this.idEstudiante=this.$route.params.id;
+        console.log(this.$route.query.anio);
+        console.log(this.$route.query.mes);
+        console.log(this.$route.query.apellido)
+    },
+    methods:{
+        botonId(){
+            this.idEstudiante=this.$route.params.id;
+        }
+    }
 }
 </script>
 
